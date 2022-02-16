e9f09dc3fe1b0d3dfab8f32f97608bfa738d828b
Joe Dei Rossi <deirossijoseph@gmail.com>
2022-02-15 18:53:01 -0700

    Completed javascript-operators-and-expressions exercise

---
+++ js/javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+
+// task 1
+var width = 8;
+var height = 4;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+// task 2
+var bill = 17;
+var payment = 20;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+// task 3
+var quizzes = 92;
+var midterm = 78;
+var final = 88;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+// task 4
+var firstName = 'Joe';
+var lastName = 'Dei Rossi';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+// task 5
+var pH = 1;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+// task 6
+var headCount = 22;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+// task 7
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
